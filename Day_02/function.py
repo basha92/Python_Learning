7fc0c2c0f721fce516f1d5563c52d144e43854d2
basha92 <sikkandarbasha1992@gmail.com>
2026-07-26 21:06:41 +0530

    function with return value

--- Day_02/function.py
+++ Day_02/function.py
@@ -7,9 +7,16 @@
 #greet()
 
 #calling funtion with parameter
-def greet(name):
-    print("Welcome, "+ name)
+#def greet(name):
+    #print("Welcome, "+ name)
 
-greet("Basha")
-greet("Alice")
-greet("Bob")
+#greet("Basha")
+#greet("Alice")
+#greet("Bob")
+
+#function with return value
+def add(a,b):
+    return a+b
+
+result = add(10,20)
+print("The sum is: ", result)
