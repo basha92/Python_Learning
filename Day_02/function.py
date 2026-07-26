777baa713543a7e5489a0951a7201c0b27cf9cbf
basha92 <sikkandarbasha1992@gmail.com>
2026-07-26 21:04:00 +0530

    function calling with parameter

--- Day_02/function.py
+++ Day_02/function.py
@@ -1,7 +1,15 @@
 #this is regarding basic function definition
-def greet():
-    print("Welcome to Python")
+#def greet():
+    #print("Welcome to Python")
 
-greet()
-greet()
-greet()
+#greet()
+#greet()
+#greet()
+
+#calling funtion with parameter
+def greet(name):
+    print("Welcome, "+ name)
+
+greet("Basha")
+greet("Alice")
+greet("Bob")
