a3acec56e6be26cf7a4e37f5eb6ead12229296a4
basha92 <sikkandarbasha1992@gmail.com>
2026-07-30 14:22:21 +0530

    added for loop

---
+++ Day_04/printnumbers.py
@@ -0,0 +1,10 @@
+#printing numbers using for loop
+#prints numbers from 1 to 10
+#for loop = execute a block of code a fixed number of times.
+#   you can iterate over range, string, sequence etc.
+for num in range(1, 11):
+    print(num)
+
+#printing in sequence of 2
+for num in range(2, 21, 2):
+    print(num)
