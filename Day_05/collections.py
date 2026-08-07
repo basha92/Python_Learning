f74caa4154d16615a59aa376397653f9a3b9be0e
basha92 <sikkandarbasha1992@gmail.com>
2026-08-07 15:05:45 +0530

    added example for list

---
+++ Day_05/collections.py
@@ -0,0 +1,19 @@
+#this program provides examples for all collection types in python
+'''
+collection - single variable used to store multiple values
+List - [] ordered and changeable; allows duplicates; any type of data can be stored;
+insert(), append(), extend(), pop()'''
+
+#example list
+fruits = ['apple', 'orange', 'coconut', 'kiwi']
+#print all elemetnts
+print(fruits)
+
+#print particular
+print(fruits[0])
+print(fruits[1])
+
+#print in range
+print(fruits[::-1]) #print from last element
+print(fruits[::2]) #print all second elements from zeroth element
+print(fruits[2:4]) #print the elemtns in the index 2,3.
