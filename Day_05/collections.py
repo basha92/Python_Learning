438c341ab147ab977c026632367cfe8f99ab3a42
basha92 <sikkandarbasha1992@gmail.com>
2026-08-07 21:21:50 +0530

    added set notes

--- Day_05/collections.py
+++ Day_05/collections.py
@@ -4,7 +4,7 @@ List - [] ordered and changeable; allows duplicates; any type of data can be sto
 insert(), append(), extend(), pop()'''
 
 #example list
-fruits = ['apple', 'orange', 'coconut', 'kiwi']
+#fruits = ['apple', 'orange', 'coconut', 'kiwi']
 #print all elemetnts
 #print(fruits)
 
@@ -24,4 +24,26 @@ extend() - add elements one by one'''
 #fruits.extend(['strawberry', 'banana']) - ['apple', 'orange', 'coconut', 'kiwi', 'strawberry', 'banana']
 #print(fruits)
 
-'''Tuples - () '''
+'''set - {} unordered and immutable. ADD/REMOVE ok. no duplicates
+common methods - add(), remove(), update(), pop()'''
+#fruits = {'apple', 'orange', 'coconut', 'kiwi', 'dragon fruit'}
+
+#adding element
+# fruits.add('banana')
+
+#updating
+#The .add() method treats whatever you pass into it as one single element.
+#The .update() method expects an iterable (a collection it can loop through). 
+#Because a string is an iterable in Python, 
+#.update() unpacks the string and attempts to add every individual character. 
+#Since sets automatically remove duplicates, only the unique letters remain.
+#fruits.update('dragon fruit')
+#o/p: {' ', 'coconut', 'a', 'd', 'kiwi', 'orange', 'apple', 'o', 'u', 'i', 't', 'r', 'g', 'f', 'n'}
+
+#removing element
+#fruits.remove('apple')
+
+#popping out the last element
+#fruits.pop()
+
+#print(fruits)
