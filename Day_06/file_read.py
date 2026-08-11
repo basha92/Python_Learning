2e02a37ba7cce896e6b24f3ba7afd20415aa5e88
basha92 <sikkandarbasha1992@gmail.com>
2026-08-11 15:56:23 +0530

    added examples for JSON and CSV reading

--- Day_06/file_read.py
+++ Day_06/file_read.py
@@ -5,17 +5,32 @@
 #reading text file
 #file_path = "Day_06/data/patients.txt"
 
+#to read contents of json
+#import json
 #reading json file
 #file_path = "Day_06/data/patients.json"
 
+#try:
+#opening the file and reading contents of JSON
+    #with open(file_path, "r") as file:
+        #reading contents of JSON
+        #content = json.load(file)
+        #print(content[0])
+        #o/p: {'name': 'John', 'age': 30, 'height': 1.75, 'weight': 72}
+
+#to read contents of CSV
+import csv
 #reading csv file
 file_path = "Day_06/data/patients.csv"
 
 try:
-#opening the file and reading contents
+#opening the file and reading contents of CSV
     with open(file_path, "r") as file:
-        content = file.read()
-        print(content)
+        #reading contents of JSON
+        content = csv.reader(file)
+        for line in content:
+            #print(line) #prints whole file
+            print(line[0]) #prints particular column with that indes
 
 except FileNotFoundError:
     print("The file doesn't exist")
